e4a66051ab868a5fbfa0c4ae8d9c69762388fc75
codeconcept <contact@codeconcept.fr>
2020-01-04 21:52:18 +0100

    Uploading image and retrieving form body

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const multer = require("multer");
+
+app.use(express.urlencoded({ extended: true }));
+
+const allowedImageMimeTypes = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/gif"
+];
+// max 10MB
+const upload = multer({
+  dest: "public",
+  limits: 10 * 1024 * 1024,
+  fileFilter: (req, file, cb) => {
+    cb(null, allowedImageMimeTypes.includes(file.mimetype));
+  }
+});
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -10,6 +28,12 @@ router.get("/", (req, res) => {
 });
 
 router.get("/images/new", (req, res) => {
+  res.render("image-form", {});
+});
+
+router.post("/images/new", upload.single("picture"), (req, res) => {
+  console.log("req.body", req.body);
+  console.log("req.file", req.file);
   res.render("image-form", {});
 });
 
