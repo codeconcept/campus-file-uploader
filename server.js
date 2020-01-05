a2ec62ec5d688bf8fb5c31a8916aa6337e6297b7
codeconcept <contact@codeconcept.fr>
2020-01-05 13:10:55 +0100

    Uploading image + displaying it

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const allowedImageMimeTypes = [
   "image/jpeg",
@@ -11,13 +14,28 @@ const allowedImageMimeTypes = [
   "image/png",
   "image/gif"
 ];
+
+let uploadedImageName = "";
+
+var storage = multer.diskStorage({
+  destination: "./public/images/",
+  filename: function(req, file, cb) {
+    crypto.pseudoRandomBytes(16, function(err, raw) {
+      if (err) return cb(err);
+      uploadedImageName = raw.toString("hex") + path.extname(file.originalname);
+      cb(null, uploadedImageName);
+    });
+  }
+});
+
 // max 10MB
 const upload = multer({
-  dest: "public",
+  dest: "public/images",
   limits: 10 * 1024 * 1024,
   fileFilter: (req, file, cb) => {
     cb(null, allowedImageMimeTypes.includes(file.mimetype));
-  }
+  },
+  storage: storage
 });
 
 app.set("view engine", "ejs");
@@ -34,7 +52,7 @@ router.get("/images/new", (req, res) => {
 router.post("/images/new", upload.single("picture"), (req, res) => {
   console.log("req.body", req.body);
   console.log("req.file", req.file);
-  res.render("image-form", {});
+  res.render("image-form", { uploadedImageName });
 });
 
 const PORT = 3000;
